475094f4e632336b9c428e5fac6f8c7060e46afe
tjking20 <tjking20@outlook.com>
2017-02-08 13:24:30 -0500

    added parallax js and css, edited appointments

---
+++ assets/js/parallax.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+
+		$(document).on("click", ".nav-link", function(){
+		    var section = $(this).attr("href");
+		    console.log(section);
+		    $("html, body").animate({
+		        scrollTop: $(section).offset().top
+		    });
+		});
+
+});
+
+
+
